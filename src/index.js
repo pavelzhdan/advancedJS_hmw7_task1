e948b68c00d2e15acc9640aae86153ebedbe1d77
Pavel Zhdanovich <zhdan21@gmail.com>
2020-05-08 15:41:47 +0300

    task1 completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,63 @@
-import './css/style.css';
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-classes-per-file */
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.level = 1;
+  }
+}
 
-import './js/app';
+class Bowman extends Character {
+  constructor(name) {
+    super(name);
+    this.class = 'Bowman';
+    this.attack = 25;
+    this.defence = 25;
+  }
+}
 
-// TODO: write your code in app.js
+class Swordsman extends Character {
+  constructor(name) {
+    super(name);
+    this.class = 'Swordsman';
+    this.attack = 40;
+    this.defence = 10;
+  }
+}
+
+class Magician extends Character {
+  constructor(name) {
+    super(name);
+    this.class = 'Swordsman';
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
+
+class Undead extends Character {
+  constructor(name) {
+    super(name);
+    this.class = 'Undead';
+    this.attack = 25;
+    this.defence = 25;
+  }
+}
+
+class Zombie extends Character {
+  constructor(name) {
+    super(name);
+    this.class = 'Undead';
+    this.attack = 40;
+    this.defence = 10;
+  }
+}
+
+class Daemon extends Character {
+  constructor(name) {
+    super(name);
+    this.class = 'Daemon';
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
